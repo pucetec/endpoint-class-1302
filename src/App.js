6a0f23b4986dd9702f96d5507f1b4c313bf6ad74
Isaac Mullo <isaacmulloaimg@gmail.com>
2024-01-13 11:03:14 -0500

    feat: [Isaac] [Isaac Mullo] endpoint exercise

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const retrieveUsers = async () => {
+      const newUsers = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      setUsers(newUsers.data);
+    };
+    retrieveUsers();
+  },[])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <tr>
+          <th>#</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Username</th>
+        </tr>
+        {users.map((user, index) => (
+        <tr>
+          <td>{user.id}</td>
+          <td>{user.name}</td>
+          <td>{user.email}</td>
+          <td>{user.username}</td>
+        </tr>
+        ))}
+      </table>
     </div>
   );
 }
