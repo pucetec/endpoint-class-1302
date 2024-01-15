fa4f33645c666d10e5f5bb2f2fe553ffb20944bd
SebastianRodas <JuanseRodas@gmail.com>
2024-01-15 17:35:47 -0500

    feat:[Endpoint][Sebastian]Endpoint

--- src/App.js
+++ src/App.js
@@ -1,38 +1,39 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";  // Ajustado el import de axios
 import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const App = () => {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
     const retrieveUsers = async () => {
-        const newUsers = await axios.get(
-          "https://jsonplaceholder.typicode.com/users"
-        );
-        setUsers(newUsers.data);
-    };
+      const newUsers = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+      );
 
+      setUsers(newUsers.data);
+    };
     retrieveUsers();
   }, []);
 
   return (
     <div className="App">
       <table>
+        <tr>
+          <th>#</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Username</th>
+        </tr>
+
+        {users.map((user, index) => (
           <tr>
-            <th>#</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Username</th>
+            <td>{user.id}</td>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+            <td>{user.username}</td>
           </tr>
-          {users.map((user) => (
-            <tr key={user.id}>
-              <td>{user.id}</td>
-              <td>{user.name}</td>
-              <td>{user.email}</td>
-              <td>{user.username}</td>
-            </tr>
-          ))}
+        ))}
       </table>
     </div>
   );
